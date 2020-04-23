4760d7e03f0f5bbe290d5c11c616dbf6570b2b7e
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-04-23 22:29:18 +0100

    Added main class

---
+++ src/Menu.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Menu extends JFrame implements ActionListener
+{
+
+
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+
+    public static void main(String args[]){
+        new Menu();
+    }
+
+}
