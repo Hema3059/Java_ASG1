ec1019679fb664473f92f626ee7ade13b686c37d
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-04-25 23:27:35 +0100

    Added Jframe

--- src/Menu.java
+++ src/Menu.java
@@ -1,11 +1,29 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
 
 public class Menu extends JFrame implements ActionListener
 {
 
+    JFrame jf;
+    public Menu()
+    {
 
+        jf=new JFrame();
+        jf.setLayout(null);
+        
+
+        jf.setTitle("Menu Page");
+        jf.setLocation(20,20);
+        jf.setSize(900,700);
+        jf.setResizable(false);
+
+        jf.getContentPane().setBackground(Color.WHITE);
+        jf.setVisible(true);
+
+    }
     public void actionPerformed(ActionEvent actionEvent) {
 
     }
@@ -15,3 +33,4 @@ public class Menu extends JFrame implements ActionListener
     }
 
 }
+
