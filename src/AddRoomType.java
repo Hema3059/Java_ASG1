0626734196c8e9be25ea1029c94aba7aabbf15ce
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-04-28 22:44:17 +0100

    added labels and button and text fields

--- src/AddRoomType.java
+++ src/AddRoomType.java
@@ -6,10 +6,26 @@ import java.awt.event.ActionListener;
 public class AddRoomType extends JFrame implements ActionListener
 {
     JFrame jf;
+    JLabel lbl1;
+    JTextField txt1;
+    JButton btn1;
     AddRoomType()
     {
         jf=new JFrame();
         jf.setLayout(null);
+
+        lbl1 = new JLabel("Enter Room Type Name*");
+        lbl1.setBounds(150,160,170,25);
+        jf.add(lbl1);
+
+        txt1=new JTextField(20);
+        txt1.setBounds(320,160,200,25);
+        txt1.setToolTipText("Enter Room Type Name");
+        jf.add(txt1);
+
+        btn1 = new JButton("Save");
+        btn1.setBounds(150,230,110,35);
+        jf.add(btn1);
 
         jf.setTitle("Add Room Type");
         jf.setLocation(20,20);
