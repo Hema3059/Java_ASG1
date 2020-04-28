a881451db07bb891efd9d84a12bc0ac54ff21752
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-04-28 22:38:33 +0100

    Added main method

--- src/AddRoomType.java
+++ src/AddRoomType.java
@@ -1,12 +1,20 @@
-import javax.swing.*;
+iimport javax.swing.*;
+iimport javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddRoomType extends JFrame implements ActionListener
 {
+    AddRoomType()
+    {
 
+    }
 
     public void actionPerformed(ActionEvent e) {
 
+    }
+    public static void main(String args[])
+    {
+        new AddRoomType();
     }
 }
