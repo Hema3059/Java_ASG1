410b8419dc3684f1cdd3d4bc9c42d1bcbf2d037a
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-04-28 22:41:07 +0100

    Added frame implementation

--- src/AddRoomType.java
+++ src/AddRoomType.java
@@ -1,13 +1,23 @@
-iimport javax.swing.*;
-iimport javax.swing.*;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddRoomType extends JFrame implements ActionListener
 {
+    JFrame jf;
     AddRoomType()
     {
+        jf=new JFrame();
+        jf.setLayout(null);
 
+        jf.setTitle("Add Room Type");
+        jf.setLocation(20,20);
+        jf.setResizable(false);
+        jf.getContentPane().setBackground(Color.white);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        jf.setBounds(0,0,screenSize.width, screenSize.height-50);
+        jf.setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
