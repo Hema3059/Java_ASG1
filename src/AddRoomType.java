6b3f5494f02be2f8fa1e5af8811ee1c7d76c1e05
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-04-28 22:35:18 +0100

    added room type class

--- src/AddRoomType.java
+++ src/AddRoomType.java
@@ -1,2 +1,12 @@
-public class AddRoomType {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AddRoomType extends JFrame implements ActionListener
+{
+
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
