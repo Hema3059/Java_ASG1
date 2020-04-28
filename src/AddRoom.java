f2d5b5e28c0d336ac3a43124f8eff109be4483f3
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-04-29 00:52:59 +0100

    added add room class

--- src/AddRoom.java
+++ src/AddRoom.java
@@ -1,2 +1,15 @@
-public class AddRoom {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AddRoom extends JFrame implements ActionListener
+{
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public static void main (String Args[])
+    {
+        new AddRoom();
+    }
 }
