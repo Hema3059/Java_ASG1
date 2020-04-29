f0715492ff5988fbf6ce55488f37b31c63fe30c9
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-04-29 12:12:58 +0100

    added main method

---
+++ src/ManageRooms.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ManageRooms extends JFrame implements ActionListener
+{
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public static void main(String args[])
+    {
+        new ManageRooms();
+    }
+}
