ac36501420598b48a13e44e498a3ead8f772a40d
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-04-29 02:07:29 +0100

    Added text fields

--- src/ManageRoomType.java
+++ src/ManageRoomType.java
@@ -6,10 +6,30 @@ import java.awt.event.ActionListener;
 public class ManageRoomType extends JFrame implements ActionListener
 {
     JFrame jf;
+    JLabel lbl1,lbl2;
+    JTextField txt1,txt2;
     ManageRoomType()
     {
         jf = new JFrame();
         jf.setLayout(null);
+
+        lbl1= new JLabel("Room Type id *");
+        lbl1.setBounds(150,120,130,25);
+        jf.add(lbl1);
+
+        txt1=new JTextField(20);
+        txt1.setBounds(320,120,200,25);
+        txt1.setToolTipText("Enter Room Type id");
+        jf.add(txt1);
+
+        lbl2 = new JLabel(" Room Type Name*");
+        lbl2.setBounds(150,160,170,25);
+        jf.add(lbl2);
+
+        txt2 = new JTextField(20);
+        txt2.setBounds(320,160,200,25);
+        txt2.setToolTipText("Enter Room Type Name");
+        jf.add(txt2);
 
         jf.setTitle("Room type management page");
         jf.setLocation(20,20);
