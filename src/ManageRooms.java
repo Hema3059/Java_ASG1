6f44f60c3c534d63b0561dc2a1fb99a5d985793b
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-04-29 12:18:23 +0100

    added Jframe implemented

--- src/ManageRooms.java
+++ src/ManageRooms.java
@@ -1,10 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ManageRooms extends JFrame implements ActionListener
 {
+    JFrame jf;
+    ManageRooms(){
+        jf = new JFrame();
+        jf.setLayout(null);
 
+        jf.setTitle("Room Management Page");
+        jf.setLocation(20,20);
+        jf.setResizable(false);
+        jf.getContentPane().setBackground(Color.white);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        jf.setBounds(0,0,screenSize.width, screenSize.height-50);
+        jf.setVisible(true);
+    }
     public void actionPerformed(ActionEvent e) {
 
     }
