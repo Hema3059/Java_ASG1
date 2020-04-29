a4038af12abd5f0530a4dd57e4b35095bb1f180a
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-04-29 02:02:57 +0100

    Added JFrame Setup

--- src/ManageRoomType.java
+++ src/ManageRoomType.java
@@ -1,11 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ManageRoomType extends JFrame implements ActionListener
 {
+    JFrame jf;
+    ManageRoomType()
+    {
+        jf = new JFrame();
+        jf.setLayout(null);
 
-
+        jf.setTitle("Room type management page");
+        jf.setLocation(20,20);
+        jf.setResizable(false);
+        jf.getContentPane().setBackground(Color.white);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        jf.setBounds(0,0,screenSize.width, screenSize.height-50);
+        jf.setVisible(true);
+    }
     public void actionPerformed(ActionEvent e) {
 
     }
