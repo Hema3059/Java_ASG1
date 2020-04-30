265a32f40c69139791d9a2b54b436f8da23322d7
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-04-30 22:23:34 +0100

    added Jframe

--- src/AddClerks.java
+++ src/AddClerks.java
@@ -1,9 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AddClerks extends JFrame implements ActionListener
 {
+    JFrame jf;
+    AddClerks(){
+        jf = new JFrame();
+        jf.setLayout(null);
+
+        jf.setTitle("Add New User");
+        jf.setLocation(20,20);
+        jf.setResizable(false);
+        jf.getContentPane().setBackground(Color.cyan);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        jf.setBounds(0,0,screenSize.width, screenSize.height-50);
+        jf.setVisible(true);
+    }
 
     public void actionPerformed(ActionEvent e) {
 
