d45c6ff40456cacdea8518c116c91170cb504693
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-04-30 22:17:34 +0100

    added main class

---
+++ src/AddClerks.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AddClerks extends JFrame implements ActionListener
+{
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public static void main(String args[]){
+        new AddClerks();
+    }
+}
