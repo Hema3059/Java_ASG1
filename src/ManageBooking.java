d73431cc54d121f6022cb47ade8817bbc80207e5
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-01 03:59:41 +0100

    Added main method for manage booking

---
+++ src/ManageBooking.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ManageBooking extends JFrame implements ActionListener
+{
+
+
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+    public static void main(String args[]){
+        new ManageBooking();
+    }
+}
