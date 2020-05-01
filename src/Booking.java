f6e8240e99d555148db8d27696d33b1402cad313
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-01 02:07:21 +0100

    Main class added

---
+++ src/Booking.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Booking extends JFrame implements ActionListener
+{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public static void main(String args[])
+    {
+
+    }
+}
