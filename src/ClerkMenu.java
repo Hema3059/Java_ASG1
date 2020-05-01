36cd6f8160e79358aa8b1a4fc40974cb6812a472
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-01 01:44:22 +0100

    added main method

---
+++ src/ClerkMenu.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClerkMenu extends JFrame implements ActionListener
+{
+
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public static void main(String args[])
+    {
+        new ClerkMenu();
+    }
+}
