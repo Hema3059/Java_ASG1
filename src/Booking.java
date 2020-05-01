5c7a92749afb5758c1b628f88b199fac8b7038e7
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-01 03:23:29 +0100

    added datepicker

--- src/Booking.java
+++ src/Booking.java
@@ -1,12 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Properties;
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.SqlDateModel;
 
 public class Booking extends JFrame implements ActionListener
 {
     JFrame jf;
-    JLabel l1,l2;
-    JTextField t2;
+    JLabel l1,l2,l3,l4;
+    JTextField t2,t3;
+    JDatePickerImpl datePicker;
+    SqlDateModel datemodel;
     Booking(){
         jf = new JFrame();
         jf.setLayout(null);
@@ -26,10 +32,34 @@ public class Booking extends JFrame implements ActionListener
         t2.setToolTipText("Enter Full Name");
         jf.add(t2);
 
+        l3 = new JLabel("Phone Number*");
+        l3.setBounds(150,200,170,25);
+        jf.add(l3);
+
+        t3=new JTextField(20);
+        t3.setBounds(320,200,250,25);t3.setToolTipText("Enter Phone Number");
+        jf.add(t3);
+
+        l4 = new JLabel("Select Date*");
+        l4.setBounds(150,240,170,25);
+        jf.add(l4);
+
+        datemodel = new SqlDateModel();
+        Properties p = new Properties();
+        p.put("text.today", "today");
+        p.put("text.month", "month");
+        p.put("text.year", "year");
+        JDatePanelImpl datePanel = new JDatePanelImpl(datemodel, p);
+        datePicker = new JDatePickerImpl(datePanel, new DatelblFormatter());
+        datemodel.setSelected(true);
+        //t5=new JTextField(20);
+        datePicker.setBounds(320,240,250,25);datePicker.setToolTipText("Date");
+        jf.add(datePicker);
+
         jf.setTitle("BOOKING");
         jf.setLocation(20,20);
         jf.setResizable(false);
-        jf.getContentPane().setBackground(Color.WHITE);
+        jf.getContentPane().setBackground(Color.white);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         jf.setBounds(0,0,screenSize.width, screenSize.height-50);
         jf.setVisible(true);
