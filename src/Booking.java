ad852395f81b8167fb5370d982b9a7f101eed75e
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-01 02:31:46 +0100

    jframe added

--- src/Booking.java
+++ src/Booking.java
@@ -1,9 +1,22 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class Booking extends JFrame implements ActionListener
 {
+    JFrame jf;
+    Booking(){
+        jf = new JFrame();
+        jf.setLayout(null);
 
+        jf.setTitle("BOOKING");
+        jf.setLocation(20,20);
+        jf.setResizable(false);
+        jf.getContentPane().setBackground(Color.WHITE);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        jf.setBounds(0,0,screenSize.width, screenSize.height-50);
+        jf.setVisible(true);
+    }
     @Override
     public void actionPerformed(ActionEvent e) {
 
