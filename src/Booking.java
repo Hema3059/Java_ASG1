abae07584ebe10a283c359f4d6e778042bce61cc
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-01 02:43:50 +0100

    label text field added

--- src/Booking.java
+++ src/Booking.java
@@ -5,9 +5,26 @@ import java.awt.event.*;
 public class Booking extends JFrame implements ActionListener
 {
     JFrame jf;
+    JLabel l1,l2;
+    JTextField t2;
     Booking(){
         jf = new JFrame();
         jf.setLayout(null);
+
+        l1=new JLabel("New Booking");
+        l1.setFont(new Font("Times New Roman",Font.BOLD,25));
+        l1.setBounds(250,50,300,40);
+        l1.setForeground(Color.blue);
+        jf.add(l1);
+
+        l2 = new JLabel("Name*");
+        l2.setBounds(150,160,170,25);
+        jf.add(l2);
+
+        t2=new JTextField(20);
+        t2.setBounds(320,160,250,25);
+        t2.setToolTipText("Enter Full Name");
+        jf.add(t2);
 
         jf.setTitle("BOOKING");
         jf.setLocation(20,20);
@@ -17,7 +34,7 @@ public class Booking extends JFrame implements ActionListener
         jf.setBounds(0,0,screenSize.width, screenSize.height-50);
         jf.setVisible(true);
     }
-    @Override
+
     public void actionPerformed(ActionEvent e) {
 
     }
