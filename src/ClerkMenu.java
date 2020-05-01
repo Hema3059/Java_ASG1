37528d221c6c19b8e86b312c63ce1e644dbd2701
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-01 01:56:16 +0100

    added frame constraction

--- src/ClerkMenu.java
+++ src/ClerkMenu.java
@@ -1,10 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ClerkMenu extends JFrame implements ActionListener
 {
+    JFrame jf;
+    ClerkMenu(){
+        jf = new JFrame();
+        jf.setLayout(null);
 
+        jf.setTitle("Clerk Menu Page");
+        jf.setLocation(20,20);
+        jf.setSize(900,700);
+        jf.setResizable(false);
+
+        jf.getContentPane().setBackground(Color.WHITE);
+        jf.setVisible(true);
+    }
 
     public void actionPerformed(ActionEvent e) {
 
