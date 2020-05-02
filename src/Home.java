a48ec4a76ed4512197a1ce137b4bd24dab90ace9
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-02 15:15:15 +0100

    Buttons added for Home Page

--- src/Home.java
+++ src/Home.java
@@ -6,9 +6,20 @@ import java.awt.event.ActionListener;
 public class Home extends JFrame implements ActionListener
 {
     JFrame jf;
+    JButton btn1,btn2;
     Home(){
         jf = new JFrame();
         jf.setLayout(null);
+
+        btn1 = new JButton("Manager  Login");
+        btn1.setBounds(150,340,110,35);
+        btn1.setToolTipText("click here for manager Login");
+        jf.add(btn1);
+
+        btn2 = new JButton("Clear");
+        btn2.setBounds(300,340,110,35);
+        btn2.setToolTipText("click here for client Login");
+        jf.add(btn2);
 
         jf.setTitle("Home Page");
         jf.setLocation(20,20);
