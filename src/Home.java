d8a6baebc1d6c2a638ee7e06fb18f6540de7c883
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-02 15:10:54 +0100

    Jframe added for home page

--- src/Home.java
+++ src/Home.java
@@ -1,9 +1,23 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Home extends JFrame implements ActionListener
 {
+    JFrame jf;
+    Home(){
+        jf = new JFrame();
+        jf.setLayout(null);
+
+        jf.setTitle("Home Page");
+        jf.setLocation(20,20);
+        jf.setResizable(false);
+        jf.getContentPane().setBackground(Color.white);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        jf.setBounds(0,0,screenSize.width, screenSize.height-50);
+        jf.setVisible(true);
+    }
 
 
     public void actionPerformed(ActionEvent e) {
