d12176e7ead17000f88cdf023c603de8077efaff
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-02 21:07:47 +0100

    DB connection and login page action

--- src/Managerlogin.java
+++ src/Managerlogin.java
@@ -2,6 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Managerlogin extends JFrame implements ActionListener
 {
@@ -10,10 +13,13 @@ public class Managerlogin extends JFrame implements ActionListener
     JTextField txt1;
     JPasswordField txt2;
     JButton btn1,btn2;
+    DBS db = null;
+    Connection con;
     Managerlogin()
     {
         jf = new JFrame();
         jf.setLayout(null);
+        db = new DBS();
 
         lbl1 = new JLabel("Welcome to Manager portal");
         lbl1.setFont(new Font("Times New Roman", Font.BOLD, 30));
@@ -41,13 +47,14 @@ public class Managerlogin extends JFrame implements ActionListener
         btn1 = new JButton("Login");
         btn1.setBounds(200, 350, 100, 35);
         jf.add(btn1);
+        btn1.addActionListener(this);
 
         btn2 = new JButton("Home");
         btn2.setBounds(350, 350, 100, 35);
         jf.add(btn2);
         btn2.addActionListener(this);
 
-        jf.setTitle("Manager Login");
+        jf.setTitle("Home");
         jf.setLocation(20, 20);
         jf.setSize(800, 600);
         jf.setResizable(false);
@@ -58,6 +65,28 @@ public class Managerlogin extends JFrame implements ActionListener
         if(e.getSource()==btn2)
         {
             new Home();
+        }
+        else if (e.getSource() == btn1) {
+            try {
+                String s = txt1.getText();
+                System.out.println(s);
+                String s1 = new String(txt2.getPassword());
+                System.out.println(s1);
+                con = db.getConnection();
+                System.out.println("connected to the database");
+                Statement stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery("select * from Users  where user_type='manager' and user_name='" + s + "' and user_pass='" + s1 + "'");
+
+                if (rs.next()) {
+                    new Menu();
+                } else {
+                    throw new Exception();
+                }
+            } catch (Exception ae) {
+                JOptionPane.showMessageDialog(null, " Please enter correct details", "WARNING", JOptionPane.ERROR_MESSAGE);
+                txt1.setText("");
+                txt2.setText("");
+            }
         }
     }
     public static void main(String args[]){
