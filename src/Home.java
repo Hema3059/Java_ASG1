425c33c1185d01da896766d9974250b94923b8a9
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-02 20:00:10 +0100

    action event added

--- src/Home.java
+++ src/Home.java
@@ -15,17 +15,19 @@ public class Home extends JFrame implements ActionListener
         btn1.setBounds(150,340,130,35);
         btn1.setToolTipText("click here for manager Login");
         jf.add(btn1);
+        btn1.addActionListener(this);
 
         btn2 = new JButton("Client Login");
         btn2.setBounds(320,340,130,35);
         btn2.setToolTipText("click here for client Login");
         jf.add(btn2);
+        btn2.addActionListener(this);
 
         jf.setTitle("Home");
         jf.setLocation(20, 20);
         jf.setSize(800, 600);
         jf.setResizable(false);
-        jf.getContentPane().setBackground(Color.cyan);
+        jf.getContentPane().setBackground(Color.white);
         jf.setVisible(true);
 
     }
@@ -33,6 +35,14 @@ public class Home extends JFrame implements ActionListener
 
     public void actionPerformed(ActionEvent e) {
 
+        if(e.getSource()==btn1)
+        {
+            new Managerlogin();
+        }
+        else if(e.getSource()==btn2)
+        {
+            new Clientlogin();
+        }
     }
     public static void main(String args[]){
         new Home();
