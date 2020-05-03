2be5e4aed85990569f4a90260db52e4587a32c3d
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-03 02:46:08 +0100

    Added main label to the page

--- src/ManageRoomType.java
+++ src/ManageRoomType.java
@@ -8,7 +8,7 @@ import java.sql.*;
 public class ManageRoomType extends JFrame implements ActionListener
 {
     JFrame jf;
-    JLabel lbl1,lbl2;
+    JLabel lbl1,lbl2,lbl3;
     JTextField txt1,txt2;
     JButton btn1,btn2;
     DefaultTableModel model = new DefaultTableModel();
@@ -23,6 +23,14 @@ public class ManageRoomType extends JFrame implements ActionListener
     {
         jf = new JFrame();
         jf.setLayout(null);
+        db = new DBS();
+
+        lbl3=new JLabel("Manage Room Type ");
+        lbl3.setFont(new Font("Times New Roman",Font.BOLD,25));
+        lbl3.setBounds(150,50,300,40);
+        lbl3.setForeground(Color.black);
+        jf.add(lbl3);
+
 
         lbl1= new JLabel("Room Type id *");
         lbl1.setBounds(150,120,130,25);
@@ -33,7 +41,7 @@ public class ManageRoomType extends JFrame implements ActionListener
         txt1.setToolTipText("Enter Room Type id");
         jf.add(txt1);
 
-        lbl2 = new JLabel(" Room Type Name*");
+        lbl2 = new JLabel("Room Type Name*");
         lbl2.setBounds(150,160,170,25);
         jf.add(lbl2);
 
@@ -45,17 +53,19 @@ public class ManageRoomType extends JFrame implements ActionListener
         btn1 = new JButton("View");
         btn1.setBounds(200,230,110,35);
         jf.add(btn1);
+        btn1.addActionListener(this);
 
         btn2 = new JButton("Delete");
         btn2.setBounds(350,230,110,35);
         jf.add(btn2);
+        btn2.addActionListener(this);
 
         scrlPane.setBounds(80,380,600,300);
         jf.add(scrlPane);
         tabGrid.setFont(new Font ("Times New Roman",0,15));
 
-        model.addColumn("Room_Type_ID");
-        model.addColumn("Room_Type_NAME");
+        model.addColumn("RoomType_ID");
+        model.addColumn("RoomType_NAME");
 
         jf.setTitle("Room type management page");
         jf.setLocation(20,20);
@@ -66,39 +76,44 @@ public class ManageRoomType extends JFrame implements ActionListener
         jf.setVisible(true);
     }
     public void actionPerformed(ActionEvent e) {
-        if (model.getRowCount() > 0) {
-            for (int i = model.getRowCount() - 1; i > -1; i--) {
-                model.removeRow(i);
+        if (e.getSource()==btn1) {
+
+
+            if (model.getRowCount() > 0) {
+                for (int i = model.getRowCount() - 1; i > -1; i--) {
+                    model.removeRow(i);
+                }
+            }
+            int r = 0;
+            try {
+                con = db.getConnection();
+                System.out.println("Connected to database.");
+                stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+                rst = stmt.executeQuery("SELECT * from room_type");
+                while (rst.next()) {
+                    model.insertRow(r++, new Object[]{rst.getString(1), rst.getString(2)});
+                }
+                con.close();
+            } catch (SQLException se) {
+                System.out.println(se);
+                JOptionPane.showMessageDialog(null, "SQL Error" + se);
             }
         }
-        int r = 0;
-        try {
-            con = db.getConnection();
-            System.out.println("Connected to database.");
-            stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            rst = stmt.executeQuery("SELECT * from typemaster");
-            while (rst.next()) {
-                model.insertRow(r++, new Object[]{rst.getString(1), rst.getString(2)});
-            }
-            con.close();
-        } catch (SQLException se) {
-            System.out.println(se);
-            JOptionPane.showMessageDialog(null, "SQL Error" + se);
-        }
-        try {
-            con = db.getConnection();
-            System.out.println("Connected to database.");
-            pst=con.prepareStatement("delete from typemaster where id='"+txt1.getText()+"' or roomtype='"+txt2.getText()+"'");
-            pst.executeUpdate();
-            JOptionPane.showMessageDialog(null,"Record is deleted.");
-            txt1.setText("");
-            txt2.setText("");
-            con.close();
-        }
-        catch(SQLException se)
+        else if  (e.getSource()==btn2)
         {
-            System.out.println(se);
-            JOptionPane.showMessageDialog(null,"SQL Error:"+se);
+            try {
+                con = db.getConnection();
+                System.out.println("Connected to database.");
+                pst = con.prepareStatement("delete from room_type where roomtype_id='" + txt1.getText() + "' or roomtype_name='" + txt2.getText() + "'");
+                pst.executeUpdate();
+                JOptionPane.showMessageDialog(null, "Record is deleted.");
+                txt1.setText("");
+                txt2.setText("");
+                con.close();
+            } catch (SQLException se) {
+                System.out.println(se);
+                JOptionPane.showMessageDialog(null, "SQL Error:" + se);
+            }
         }
     }
     public static void main(String args[])
