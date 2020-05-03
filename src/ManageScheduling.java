17d7297ea5072e6be2333892a177846110d08a7d
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-03 16:57:54 +0100

    Labeling and drop down menu for available room

--- src/ManageScheduling.java
+++ src/ManageScheduling.java
@@ -6,10 +6,29 @@ import java.awt.event.ActionListener;
 public class ManageScheduling extends JFrame implements ActionListener
 {
     JFrame jf;
+    JLabel lbl1,lbl2;
+    JComboBox cmb1;
     ManageScheduling()
     {
         jf = new JFrame();
         jf.setLayout(null);
+
+        lbl1=new JLabel("Scheduling Room");
+        lbl1.setFont(new Font("Times New Roman",Font.BOLD,25));
+        lbl1.setBounds(150,50,300,40);
+        lbl1.setForeground(Color.black);
+        jf.add(lbl1);
+
+        lbl2 = new JLabel("Select Room *");
+        lbl2.setBounds(150,120,130,25);
+        jf.add(lbl2);
+
+        cmb1=new JComboBox();
+        cmb1.setBounds(320,120,200,25);
+        cmb1.setToolTipText("Select Room");
+        jf.add(cmb1);
+        cmb1.addItem("Select Room");
+
 
         jf.setTitle("Add Schedule Type");
         jf.setLocation(20,20);
