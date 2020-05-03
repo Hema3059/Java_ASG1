2863fb5758fc193104223716f1162eb35ee9c6e1
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-03 16:30:53 +0100

    Changes in the menu options

--- src/Menu.java
+++ src/Menu.java
@@ -15,34 +15,34 @@ public class Menu extends JFrame implements ActionListener
         jf.setLayout(null);
 
         btn0 = new JButton("Add New Room Type");
-        btn0.setBounds(50,390,160,35);
+        btn0.setBounds(50,200,160,35);
         btn0.addActionListener(this);
         jf.add(btn0);
 
         btn1 = new JButton("Add New Room");
-        btn1.setBounds(220,390,160,35);
+        btn1.setBounds(220,200,160,35);
         btn1.addActionListener(this);
         jf.add(btn1);
 
         btn2 = new JButton("Manage Room Type");
-        btn2.setBounds(480,390,160,35);
+        btn2.setBounds(480,200,160,35);
         btn2.addActionListener(this);
         jf.add(btn2);
 
         btn3 = new JButton("Manage rooms");
-        btn3.setBounds(650,390,160,35);
+        btn3.setBounds(650,200,160,35);
         btn3.addActionListener(this);
         jf.add(btn3);
 
-        btn4 = new JButton("Add clerks");
-        btn4.setBounds(50,200,160,35);
+        btn4 = new JButton("Schedule the room");
+        btn4.setBounds(350,350,160,35);
         btn4.addActionListener(this);
         jf.add(btn4);
 
-        btn5 = new JButton("Manage clerks");
-        btn5.setBounds(220,200,160,35);
-        btn5.addActionListener(this);
-        jf.add(btn5);
+        //btn5 = new JButton("Manage clerks");
+        //btn5.setBounds(220,200,160,35);
+        //btn5.addActionListener(this);
+        //jf.add(btn5);
 
 
 
