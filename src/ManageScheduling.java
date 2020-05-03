0e5eb93fe5f6aca322282caa5c012f97053e7a73
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-03 17:59:57 +0100

    Actions to the scheduling button

--- src/ManageScheduling.java
+++ src/ManageScheduling.java
@@ -14,9 +14,10 @@ public class ManageScheduling extends JFrame implements ActionListener
     JFrame jf;
     JLabel lbl1,lbl2,lbl3;
     JComboBox cmb1,cmb2;
-    JButton btn1;
+    JButton btn1,btn2;
     DBS db = null;
     Connection con;
+    PreparedStatement pst;
     JDatePickerImpl datePk;
     SqlDateModel datemdl;
     ManageScheduling()
@@ -91,6 +92,12 @@ public class ManageScheduling extends JFrame implements ActionListener
         jf.add(btn1);
         btn1.addActionListener(this);
 
+        btn2= new JButton("All");
+        btn2.setBounds(450,320,110,35);
+        btn2.setToolTipText("click to view all  details");
+        jf.add(btn2);
+        btn2.addActionListener(this);
+
         jf.setTitle("Add Schedule");
         jf.setLocation(20,20);
         jf.setResizable(false);
@@ -101,7 +108,42 @@ public class ManageScheduling extends JFrame implements ActionListener
     }
 
 
-    public void actionPerformed(ActionEvent actionEvent) {
+    public void actionPerformed(ActionEvent ae)
+    {
+        if(ae.getSource()==btn1)
+        {
+            if((cmb1.getSelectedItem().toString().equals("Select Room")) || (cmb2.getSelectedItem().toString().equals("Select Day Type")) || datePk.getModel().getValue()=="")
+            {
+                JOptionPane.showMessageDialog(this,"* Detail are Missing !","Warning!!!",JOptionPane.WARNING_MESSAGE);
+            }
+
+            else
+            {
+                try
+                {
+                    con=db.getConnection();
+                    System.out.println("Connected to database.");
+
+                    pst=con.prepareStatement("insert into room_scheduling (room,s_date,d_type) values(?,?,?)");
+                    pst.setString(1,cmb1.getSelectedItem().toString());
+                    pst.setDate(2, (java.sql.Date) datePk.getModel().getValue());
+                    pst.setString(3,cmb1.getSelectedItem().toString());
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(this,"Room Scheduled Successfully!!","Note!!!",JOptionPane.INFORMATION_MESSAGE);
+                    con.close();
+                }
+                catch(SQLException se)
+                {
+                    System.out.println(se);
+                    JOptionPane.showMessageDialog(null,"SQL Error:"+se);
+                }
+                catch(Exception e)
+                {
+                    System.out.println(e);
+                    JOptionPane.showMessageDialog(null,"Error:"+e);
+                }
+            }
+        }
 
     }
     public static void main(String args[])
