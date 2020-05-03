0ca1ef80b66242484132d63a5ba58fd797c74425
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-03 16:36:51 +0100

    This class is build to schedule the available rooms once the room is added they can schedule the room using this class

---
+++ src/ManageScheduling.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ManageScheduling extends JFrame implements ActionListener
+{
+
+
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+    public static void main(String args[])
+    {
+        new ManageScheduling();
+    }
+}
