9f4e56fe564aecd18307f6f7bf925e97d80a76d9
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-04 20:54:57 +0100

    background color Changed

--- src/ClerkMenu.java
+++ src/ClerkMenu.java
@@ -6,20 +6,31 @@ import java.awt.event.ActionListener;
 public class ClerkMenu extends JFrame implements ActionListener
 {
     JFrame jf;
-    JButton btn0,btn1;
+    JButton btn0,btn1,btn2;
+    JLabel lbl1;
     ClerkMenu(){
         jf = new JFrame();
         jf.setLayout(null);
 
+        lbl1 = new JLabel("Welcome to Clerk login");
+        lbl1.setFont(new Font("Times New Roman", Font.BOLD, 30));
+        lbl1.setBounds(280, 120, 400, 40);
+        jf.add(lbl1);
+
         btn0 = new JButton("New Booking");
-        btn0.setBounds(50,200,160,35);
+        btn0.setBounds(350,200,160,35);
         btn0.addActionListener(this);
         jf.add(btn0);
 
         btn1 = new JButton("Manage Booking");
-        btn1.setBounds(220,200,160,35);
+        btn1.setBounds(350,300,160,35);
         btn1.addActionListener(this);
         jf.add(btn1);
+
+        btn2 = new JButton("List the rooms");
+        btn2.setBounds(350,400,160,35);
+        btn2.addActionListener(this);
+        jf.add(btn2);
 
 
         jf.setTitle("Clerk Menu Page");
@@ -27,7 +38,7 @@ public class ClerkMenu extends JFrame implements ActionListener
         jf.setSize(900,700);
         jf.setResizable(false);
 
-        jf.getContentPane().setBackground(Color.WHITE);
+        jf.getContentPane().setBackground(Color.lightGray);
         jf.setVisible(true);
     }
 
@@ -39,6 +50,10 @@ public class ClerkMenu extends JFrame implements ActionListener
         else if(e.getSource()==btn1)
         {
             new ManageBooking();
+        }
+        else if(e.getSource()==btn2)
+        {
+            new List();
         }
 
     }
