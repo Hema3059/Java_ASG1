ea74beff85939474f131176b0202201cea737870
HEMALATHA898 <hemalathakancheti@gmail.com>
2020-05-04 20:32:04 +0100

    UI design modified

--- src/ManageRoomType.java
+++ src/ManageRoomType.java
@@ -27,40 +27,40 @@ public class ManageRoomType extends JFrame implements ActionListener
 
         lbl3=new JLabel("Manage Room Type ");
         lbl3.setFont(new Font("Times New Roman",Font.BOLD,25));
-        lbl3.setBounds(150,50,300,40);
+        lbl3.setBounds(340,50,300,40);
         lbl3.setForeground(Color.black);
         jf.add(lbl3);
 
 
         lbl1= new JLabel("Room Type id *");
-        lbl1.setBounds(150,120,130,25);
+        lbl1.setBounds(240,120,130,25);
         jf.add(lbl1);
 
         txt1=new JTextField(20);
-        txt1.setBounds(320,120,200,25);
+        txt1.setBounds(400,120,200,25);
         txt1.setToolTipText("Enter Room Type id");
         jf.add(txt1);
 
         lbl2 = new JLabel("Room Type Name*");
-        lbl2.setBounds(150,160,170,25);
+        lbl2.setBounds(240,160,170,25);
         jf.add(lbl2);
 
         txt2 = new JTextField(20);
-        txt2.setBounds(320,160,200,25);
+        txt2.setBounds(400,160,200,25);
         txt2.setToolTipText("Enter Room Type Name");
         jf.add(txt2);
 
         btn1 = new JButton("View");
-        btn1.setBounds(200,230,110,35);
+        btn1.setBounds(330,230,110,35);
         jf.add(btn1);
         btn1.addActionListener(this);
 
         btn2 = new JButton("Delete");
-        btn2.setBounds(350,230,110,35);
+        btn2.setBounds(500,230,110,35);
         jf.add(btn2);
         btn2.addActionListener(this);
 
-        scrlPane.setBounds(80,380,600,300);
+        scrlPane.setBounds(150,380,600,300);
         jf.add(scrlPane);
         tabGrid.setFont(new Font ("Times New Roman",0,15));
 
@@ -70,10 +70,11 @@ public class ManageRoomType extends JFrame implements ActionListener
         jf.setTitle("Room type management page");
         jf.setLocation(20,20);
         jf.setResizable(false);
-        jf.getContentPane().setBackground(Color.white);
+        jf.getContentPane().setBackground(Color.lightGray);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         jf.setBounds(0,0,screenSize.width, screenSize.height-50);
         jf.setVisible(true);
+        jf.setBounds(50,100, 1000, 1200);
     }
     public void actionPerformed(ActionEvent e) {
         if (e.getSource()==btn1) {
